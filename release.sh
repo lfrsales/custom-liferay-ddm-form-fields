f4bcac799ec1dca90ec9f33862f57d3e85414603
Evan Thibodeau <evan.thibodeau@liferay.com>
2021-04-08 13:18:49 +0400

    Add release script.

---
+++ release.sh
@@ -0,0 +1,6 @@
+hub release create \
+	--attach modules/character-limited-text/build/libs/com.liferay.dynamic.data.mapping.form.field.type.internal.character.limited.text-1.0.0.jar \
+	--attach modules/userdata-field/build/libs/com.liferay.dynamic.data.mapping.form.field.type.internal.userdata.field-1.0.0.jar \
+	--message 'v0.0.2' \
+	--message 'Include Martins userdata field.' \
+	'v0.0.2'

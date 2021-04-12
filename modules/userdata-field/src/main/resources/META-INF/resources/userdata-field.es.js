f7be83b532d04b91c3f9817b6c4c08a87b3ae4fd
Evan Thibodeau <evan.thibodeau@liferay.com>
2021-04-12 11:40:18 +0400

    Revert "Reorganize js and only call onChange on user input."
    This reverts commit 7d4d12e0f420eda1c619b7dd123c51034a5b1af8.

--- modules/userdata-field/src/main/resources/META-INF/resources/userdata-field.es.js
+++ modules/userdata-field/src/main/resources/META-INF/resources/userdata-field.es.js
@@ -7,74 +7,52 @@ import {ClayInput} from '@clayui/form';
  * UserData Input React Component
  */
 
-const UserDataFieldDDMFormFieldType = (props) => {
-	const {
-		disabled,
-		id,
-		name,
-		onBlur,
-		onChange,
-		onFocus,
-		value: initialValue,
-	} = props;
-
-	const [value, setValue] = useSyncValue(initialValue, true);
-
-	useEffect(() => {
-		setValue(initialValue);
-	}, [initialValue, setValue]);
-
-	return (
-		<ClayInput
-			className="ddm-field-text"
-			disabled={disabled}
-			id={id ? id : name}
-			name={name}
-			onBlur={onBlur}
-			onChange={(event) => {
-				setValue(event.target.value);
-
-				onChange(event);
-			}}
-			onFocus={onFocus}
-			type="text"
-			value={value}
-		/>
-	);
-};
+const UserDataFieldDDMFormFieldType = ({name, onChange, predefinedValue, readOnly, value}) => (
+	<ClayInput
+		className="ddm-field-text"
+		disabled={readOnly}
+		name={name}
+		onInput={onChange}
+		type="text"
+		value={value ? value : predefinedValue}
+	/>
+);
 
 const Main = props => {
 	const {
-		disabled,
-		id,
 		label,
 		name,
-		onBlur,
 		onChange,
-		onFocus,
 		predefinedValue = '',
 		readOnly,
 		value,
 		...otherProps
 	} = props;
 
+	const [currentValue, setCurrentValue] = useSyncValue(
+		value ? value : predefinedValue
+	);
+
+	useEffect(() => {
+		setCurrentValue(predefinedValue);
+		onChange({target: {value: predefinedValue}});
+	}, []);
+
 	return <FieldBase
-			{...otherProps}
-			id={id}
 			label={label}
 			name={name}
-			readOnly={readOnly}
+			predefinedValue={predefinedValue}
+			{...otherProps}
 		>
 			<UserDataFieldDDMFormFieldType
-				disabled={disabled || readOnly}
-				id={id}
-				label={label}
 				name={name}
-				onBlur={onBlur}
-				onChange={onChange}
-				onFocus={onFocus}
+				onChange={(event) => {
+					setCurrentValue(event.target.value);
+					onChange(event);
+				}}
+				readOnly={readOnly}
 				predefinedValue={predefinedValue}
-				value={value ? value : predefinedValue}
+				value={currentValue}
 			/>
 		</FieldBase>
 }
